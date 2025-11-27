636945439e0d62dd330fef2675c8a027b626486d
Paul Gaulmin <paul.gaulmin@ynov.com>
2025-11-27 12:14:48 +0100

    ajout des routes

--- routeur/routeur.go
+++ routeur/routeur.go
@@ -10,6 +10,13 @@ func New() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", controller.Home)
-	//a modifier pour mettre bien tout les chemins des pages des qu'on aura tt les pages
-	//mux.HandleFunc("...", controller."")
+	mux.HandleFunc("/aPropos", controller.aPropos)
+	mux.HandleFunc("/categories", controller.categories)
+	mux.HandleFunc("/collection", controller.collection)
+	mux.HandleFunc("/favoris", controller.favoris)
+	mux.HandleFunc("/recherche", controller.recherche)
+	mux.HandleFunc("/resssources", controller.resssources)
+	//normalement ya tt les pages
+
+	return mux
 }
